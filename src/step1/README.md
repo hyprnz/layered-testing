01964801a2d37f641332a47d636b1382f189a98b
hyprg <gareth.evans@hypr.co.nz>
2018-06-21 15:13:39 +1200

    Added some steps.

---
+++ src/step1/README.md
@@ -0,0 +1,19 @@
+# Test what you change
+
+Your customers could greatly benefit from enhancing the capabilities of the legacy calculator.
+
+Currently the calculator processes a collection of dates to calculate a planned start date.
+
+There is a new market segment which need a different algorithm to calculate the planned start date.
+
+Our goal is to refactor the existing code to a point where it is clearer where the new algorithm can be introduced.
+
+## Step one 
+
+Look at the code and identify refactorings that make the code easier to read and understand.
+
+Why is the legacy calculator doing too much?
+
+What are some of the concerns that could be split out?
+
+We spend 60-70% of our time as developers reading code, why is this code hard to read and understand?
--- src/step2/LegacyCalculator.ts
+++ src/step2/LegacyCalculator.ts
@@ -1,17 +1,11 @@
 import {PlannedStart} from "../PlannedStart";
 
-// Legacy Calculator calculates a planned start date from dates.
 export class LegacyCalculator {
-
-    // Calculate method returns a planned start date from dates and required days.
-    // Last updated by Bob 2014-09-21 to introduce counts for second week logic.
     calculate(dates: Array<Date>, requiredDays = 1): PlannedStart {
-        // Sort the dates
         dates.sort((a, b,) => a.getTime() - b.getTime());
 
         let plannedStart = {startTime: 0, count: 0};
 
-        // check if dates no items then return early
         if (dates.length === 0) {
             return plannedStart;
         }
@@ -19,15 +13,14 @@ export class LegacyCalculator {
         const requiredNumberInFirstWeek = requiredDays;
 
         const startOfFirstWeek = dates[0].getTime();
-        // add a week
         const startOfSecondWeek = startOfFirstWeek + (7 * 24 * 60 * 60 * 1000);
 
         const countsForFirstWeek = dates
-            .filter(x => x.getTime() > startOfFirstWeek && x.getTime() < startOfFirstWeek + (7 * 24 * 60 * 60 * 1000)) // add a week
+            .filter(x => x.getTime() > startOfFirstWeek && x.getTime() < startOfFirstWeek + (7 * 24 * 60 * 60 * 1000))
             .length;
 
         const countsForSecondWeek = dates
-            .filter(x => x.getTime() > startOfSecondWeek && x.getTime() < startOfSecondWeek + (7 * 24 * 60 * 60 * 1000)) // add a week
+            .filter(x => x.getTime() > startOfSecondWeek && x.getTime() < startOfSecondWeek + (7 * 24 * 60 * 60 * 1000))
             .length;
 
         if (countsForSecondWeek > countsForFirstWeek && countsForSecondWeek >= requiredNumberInFirstWeek) {
